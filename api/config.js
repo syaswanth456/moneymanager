9539fa39f60e81ddb89ce9c544b68c5b2411fc71
syaswanth456 <syaswanthkumar@hotmail.com>
2026-01-03 21:21:11 +0530

    Add api/config.js file

---
+++ api/config.js
@@ -0,0 +1,7 @@
+// inside server.js (or in api/config.js router)
+app.get('/api/config', (req, res) => {
+  res.json({
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY
+  });
+});
