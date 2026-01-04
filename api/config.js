86548b7548bb53a5af064ad3eba23308cd100132
syaswanth456 <syaswanthkumar@hotmail.com>
2026-01-04 11:46:38 +0530

    Delete api directory

--- api/config.js
+++
@@ -1,7 +0,0 @@
-// inside server.js (or in api/config.js router)
-app.get('/api/config', (req, res) => {
-  res.json({
-    supabaseUrl: process.env.SUPABASE_URL,
-    supabaseAnonKey: process.env.SUPABASE_ANON_KEY
-  });
-});
